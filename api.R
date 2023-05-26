28655023fbea9f0a63adbe88c871c17a0ec9b3a4
ainhize barredo <ainhize.barredo@alumni.mondragon.edu>
2023-05-26 19:59:28 +0200

    mi SEGUNDO commit

---
+++ api.R
@@ -0,0 +1,18 @@
+#* @apiTitle Restaurantes de NY
+#* @apiDescription a traves de esta api podras saber la cantidad de restaurantes que hay para cada tipo de comida y de servicio a domicilio
+
+
+#* @param tipo_comida TIPO DE COMIDA
+#* @get /cantidad 
+
+function(tipo_comida=""){
+  df_comida<-df%>%
+    filter(df$Popular.food==tipo_comida)
+  cantidad<-dim(df_comida)[1]
+  paste0('hay ', cantidad, ' de restaurantes para el tipo de comida: ', tipo_comida)
+}
+
+#* @param domicilio A DOMICILIO?
+#* @get /domicilio
+
+ 
